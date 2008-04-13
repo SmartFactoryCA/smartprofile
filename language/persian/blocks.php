aea172fdaa6d4b1a12ffa20ad2bc10150e820cc3
Sina Asghari <stranger@impresscms.org>
2008-04-13 09:31:29 +0000

    Translating a couple of modules to Persian (smartprofile) & (smartpartner) & (smartmedia) & (members) and removing all Thumbs.db files from modules.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/smartprofile/trunk@1556 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/persian/blocks.php
@@ -0,0 +1,6 @@
+<?php
+
+// Translated by stranger <www.impresscms.ir>
+define("_MB_SPROFILE_DISPLAY", "نمایش %s کاربر");
+define("_MB_SPROFILE_DISPLAYA", "نمایش شکلک کاربران");
+?>
---
+++ language/persian/error.php
@@ -0,0 +1,5 @@
+<?php
+// $Id: error.php,v 1.2 2006/12/07 20:42:30 mithyt2 Exp $
+// Translated by stranger <www.impresscms.ir>
+define("PROFILE_ERROR1", "کليد اشتباه مي باشد");
+?>
